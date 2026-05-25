c06ba020e37a76c1a442662db29dac34d43d96dc
madan123051 <madan123051@gmail.com>
2026-05-25 22:07:59 +0900

    fix: add force-dynamic to prevent Firebase prerender crash at build time

--- src/app/embed/page.tsx
+++ src/app/embed/page.tsx
@@ -2,6 +2,8 @@
  * Embeddable community page for use in iframes from wildsaura.com, drishya, market.wildsaura.com
  * Usage: <iframe src="https://community.wildsaura.com/embed" />
  */
+export const dynamic = 'force-dynamic';
+
 import { CommunityPage } from '@/components/community/CommunityPage';
 
 export default function EmbedPage() {
